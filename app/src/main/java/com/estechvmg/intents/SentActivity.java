9596d001f1170636c8caf8b8b6e13be93b4c26ae
esTechVMG <vicente.maroto@escuelaestech.es>
2020-03-11 00:22:55 +0100

    Finished

--- app/src/main/java/com/estechvmg/intents/SentActivity.java
+++ app/src/main/java/com/estechvmg/intents/SentActivity.java
@@ -1,11 +1,18 @@
 package com.estechvmg.intents;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SentActivity extends AppCompatActivity {
-    public TextView welcome;
+    public TextView welcome,counter;
+    public Button reset,sum;
+    public LinearLayout lay;
+    private int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +21,34 @@ public class SentActivity extends AppCompatActivity {
         String name=b.getString("NAME");
         welcome=findViewById(R.id.welcome);
         welcome.setText("Welcome " + name);
-
+        sum=findViewById(R.id.addToCount);
+        reset=findViewById(R.id.reset);
+        lay=findViewById(R.id.lay);
+        counter=findViewById(R.id.counter);
+        updateCounter();
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                updateCounter();
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count=0;
+                updateCounter();
+            }
+        });
+    }
+    private void updateCounter() {
+        counter.setText(String.valueOf(count));
+        if (count<=10){
+            lay.setBackgroundColor(Color.GREEN);
+        }else if(count <= 50){
+            lay.setBackgroundColor(Color.YELLOW);
+        }else{
+            lay.setBackgroundColor(Color.RED);
+        }
     }
 }
